fb2ed59c22331b1adda022401ab1d2087a30f690
mohamed96321 <atefm.me499@gmail.com>
2023-11-20 18:27:08 +0200

    Styling

--- assets/js/app.js
+++ assets/js/app.js
@@ -79,7 +79,7 @@ class TaskListView {
 
     this.container.innerHTML = `
       <div class="flex flex-col items-center mb-2">
-        <h1 class="text-4xl font-bold mb-4 mt-6 text-gray-800">TODO APP</h1>
+        <h1 class="text-4xl font-bold mb-2 mt-3 text-gray-800">TODO APP</h1>
         <div class="w-full max-w-md bg-white rounded-lg shadow-xl p-6 mb-4">
           <div class="flex">
             <input id="taskInput" type="text" placeholder="Enter a task" class="border border-gray-300 px-4 py-2 rounded-lg w-full mr-2 focus:outline-none" />
